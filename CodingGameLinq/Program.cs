f9c0246379e64f52af3e355a875593545f7a7b92
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-07-17 12:32:21 +0300

    Add Exercise 2 (Query Syntax Exercise)'s solution

--- CodingGameLinq/Program.cs
+++ CodingGameLinq/Program.cs
@@ -5,4 +5,16 @@ public static class Program
     // Exercise 1:
     // In this exercise, write a lambda expression that returns the provided value plus one.
     public static Func<int, int> GetNextNumber = num => num + 1;
+    
+    // Exercise 2 (Query Syntax Exercise):
+    // Now you try it out. The following code shows a LINQ query that returns the input inValues unmodified.
+    // Using the format of the example above, see if you can return only the strings that have
+    // pattern in them (using the String.Contains() method) and order the list alphabetically.
+    public static IEnumerable<string> FilterAndSortQuery(IEnumerable<string> inValues, string pattern)
+    {
+        return from value in inValues
+            where value.Contains(pattern)
+            orderby value
+            select value;
+    }
 }
