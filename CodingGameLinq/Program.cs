ff1c3fdb7b860c5ba224c0154c6bc218aa556582
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-07-17 12:34:42 +0300

    Add Exercise 4 (Extract a Single Element)'s solution

--- CodingGameLinq/Program.cs
+++ CodingGameLinq/Program.cs
@@ -30,4 +30,33 @@ public static class Program
                 .OrderBy(value => value)
             ;
     }
+    
+    // Exercise 4 (Extract a single element):
+    // Using what you have learned about First(), Last(), ElementAt(), and Single() (and their variations) modify
+    // the methods in this exercise to extract the desired element from the provided sequence of strings.
+    public static string GetFirstSingleLetterWord(IEnumerable<string> words)
+    {
+        return words.First(word => word.Length == 1);
+    }
+
+    // Return the last word that contains the substring "her" in it.
+    // There will always be at least one.
+    public static string GetLastWordWithHerInIt(IEnumerable<string> words)
+    {
+        return words.Last(word => word.Contains("her"));
+    }
+
+    // Return the fifth word in the sequence, if there is one. If there are
+    // fewer than 5 words, then return null.
+    public static string GetFifthWordIfItExists(IEnumerable<string> words)
+    {
+        return words.ElementAtOrDefault(4);
+    }
+
+    // Return the last word in the sequence. If there are no words in
+    // the sequence, return null.
+    public static string GetLastWordIfAny(IEnumerable<string> words)
+    {
+        return words.LastOrDefault();
+    }
 }
