e72636f8f6e241ed1e5ac84ccbffd7d59d6d4403
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-07-17 12:56:17 +0300

    Add Exercise 1 (Delegates Exercise)'s solution

--- CodingGameLinq/Program.cs
+++ CodingGameLinq/Program.cs
@@ -2,4 +2,17 @@
 
 public static class Program
 {
+    // Exercise 1 (Delegates Exercise):
+    // Declare a new type SayHello as a delegate that takes a single string parameter and returns a string result.
+    public static void PrintAnswer(string func)
+    {
+        Console.WriteLine(func);
+    }
+    public delegate string SayHello(string str);
+    public static void CallSayHelloDelegate(SayHello func)
+    {
+        PrintAnswer(func("World"));
+        PrintAnswer(func("my baby"));
+        PrintAnswer(func("my honey"));
+    }
 }
