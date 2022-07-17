372733266ae90ed0f65ef038fbbb0c582a762efc
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-07-17 13:05:51 +0300

    Add Exercise 4 (Extension Methods Exercise)'s solution

---
+++ CodingGameLinq/StringExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace CodingGameLinq;
+
+// Exercise 4 (Extension Methods Exercise):
+// In this exercise, you must add an extension method, SayHello() to the built-in string type.
+// The SayHello() method should return the string: "Hello, <subject>!"
+public static class StringExtensions
+{
+    public static string SayHelloExercise4(this string str) => $"Hello, {str}!";
+}
