3e975bc7f424e0fe52003a746a6a9476ac61b258
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-07-17 12:30:36 +0300

    Add Exercise 1 Solution

--- CodingGameLinq/Program.cs
+++ CodingGameLinq/Program.cs
@@ -2,4 +2,7 @@
 
 public static class Program
 {
+    // Exercise 1:
+    // In this exercise, write a lambda expression that returns the provided value plus one.
+    public static Func<int, int> GetNextNumber = num => num + 1;
 }
