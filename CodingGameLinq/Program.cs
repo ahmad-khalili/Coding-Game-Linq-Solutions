ce1fe273ad4029f3784c1814d1a81603851b1ede
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-07-17 13:12:19 +0300

    Add Exercise 7 (Bounded Generator Exercise)'s solution

--- CodingGameLinq/Program.cs
+++ CodingGameLinq/Program.cs
@@ -49,7 +49,7 @@ public static class Program
      0+2, 2x2, 4+2, 6x2, 12+2, 14x2, 28+2, 30x2, 60+2 ...
      = 2,   4,   6,   12,  14,   28,   30,   60,   62 ...
     */
-    public static IEnumerable<int> AddTwoThenDouble(){
+    public static IEnumerable<int> AddTwoThenDoubleUnbounded(){
         int latestValue = 0;
         bool isAdd = true;
             
@@ -57,6 +57,28 @@ public static class Program
             latestValue = (isAdd ? latestValue + 2 : latestValue * 2);
             isAdd = !isAdd;
             yield return latestValue;
+        }
+    }
+    
+    // Exercise 7 (Bounded Generator Exercise):
+    // Exercise time again! Let's see if you can write a bounded generator.
+    // In this exercise, you will implement the same generator as in the unbounded generator exercise,
+    // but this time the sequence will be limited to the first n values, where n is a parameter to the method.
+    // As a reminder, here is the sequence you will be implementing:
+    /* Alternate between adding 2 to the previous number, then multiplying the previous number by 2.
+     Here are the first several values in this sequence:
+     0+2, 2x2, 4+2, 6x2, 12+2, 14x2, 28+2, 30x2, 60+2 ...
+     = 2,   4,   6,   12,  14,   28,   30,   60,   62 ...
+     */
+    public static IEnumerable<int> AddTwoThenDoubleBounded(int n)
+    {
+        int value = 0;
+        bool isAdd = true;
+        for(int i = 0; i < n;i++)
+        {
+            value = (isAdd ? value + 2 : value * 2);
+            isAdd = !isAdd;
+            yield return value;
         }
     }
 }
